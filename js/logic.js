cbaedb7460636cea438eb60ffe20ac26465d6c6d
Your Name <wuyang@192-168-1-101.tpgi.com.au>
2021-01-08 15:42:59 +1100

    recomment things to make codes more readable

--- js/logic.js
+++ js/logic.js
@@ -4,7 +4,7 @@ const checkWin = function (arr) {
   let win = 0;
   for (i = 0; i < winOccasion.length; i++) {
     if (winOccasion[i].every (n => arr.includes(n))) {
-      win ++;//check if one of the situations can be fulfilled by player's choices
+      win ++;//check if one of the situations is contained in one player's choices
       break
     }
   }
@@ -15,7 +15,7 @@ const checkWin = function (arr) {
   }
 }
 
-const draw = function () {//draw situation:9 cells have all been chosen but no one wins.
+const draw = function () {//draw situation=9 cells have all been chosen but no one wins.
   let chosen = game.player1.concat(game.player2);
   if(chosen.length === 9 && game.result.length === 0) {
     game.result = "It's a draw!"
@@ -23,8 +23,8 @@ const draw = function () {//draw situation:9 cells have all been chosen but no o
 }//why directly compare player1+player2 with choices cannot work?
 
 const game = {
-  choices: [1, 2, 3, 4, 5, 6, 7, 8, 9],//make every element only be selected once
-  player1: [],
+  choices: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  player1: [],//store choices made in one round of a game
   player2: [],
 
   player1Win: 0,//store the winning record
